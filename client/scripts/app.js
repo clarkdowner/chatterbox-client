f3d372d010498c5287c77bdfa10fc67f46fb1d4e
clarkdowner <cddowner@gmail.com>
2016-09-26 15:04:15 -0700

    Implement fetch method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,23 @@
 class App {
-  constructor() {
-
+  constructor(server) {
+    this.server = server;
   }
   init() {
 
   }
   send(message) {
     $.ajax({
-      url: 'url',
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message)
     });
   }
+  fetch() {
+    $.ajax({
+      url: this.server,
+      type: 'GET'
+    });
+  }
 }
 
-var app = new App();
+var app = new App('https://api.parse.com/1/classes/messages');
