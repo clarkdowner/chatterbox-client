5408eb1e5ee6cc2bb38a7ab0dafb876e4f512e7e
clarkdowner <cddowner@gmail.com>
2016-09-26 14:59:54 -0700

    Implement send method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE:
+class App {
+  constructor() {
+
+  }
+  init() {
+
+  }
+  send(message) {
+    $.ajax({
+      url: 'url',
+      type: 'POST',
+      data: JSON.stringify(message)
+    });
+  }
+}
+
+var app = new App();
